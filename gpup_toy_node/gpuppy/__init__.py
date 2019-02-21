5cf40cb9b83b31505a6838272dbedd3b746d35bb
avishais <avishai.sintov@gmail.com>
2019-02-21 09:47:00 -0500

    Deleted old toy node

--- gpup_toy_node/gpuppy/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (C) 2015 Philipp Baumgaertel
-# All rights reserved.
-#
-# This software may be modified and distributed under the terms
-# of the BSD license.  See the LICENSE.txt file for details.
