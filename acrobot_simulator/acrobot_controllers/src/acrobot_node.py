ba8d171caed0ceab93407722885bf53993c0aae2
avishais <avishai.sintov@gmail.com>
2019-03-03 21:42:36 -0500

    Acrobot controller

---
+++ acrobot_simulator/acrobot_controllers/src/acrobot_node.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python 
+
+'''
+----------------------------
+Author: Avishai Sintov
+        Rutgers University
+Date: March 2019
+----------------------------
+'''
+
+
+import rospy
+import numpy as np 
+from std_msgs.msg import Float64, String
+from acrobot_controllers.srv import moveservo
+from std_msgs.msg import Float64MultiArray, Float32MultiArray, Float32
+from std_srvs.srv import Empty, EmptyResponse
+
+class SimAcrobotNode():
+
+    act_load = 0.
+    act_torque = 0.
+    act_angles = np.array([0,0]) # actuator angles of all three fingers
+    # fingers_angles = np.array([0.,0.,0.,0.]) # left_proximal, left_distal, right_proximal, right_distal
+    act_vel = np.array([0,0]) # left_proximal, left_distal, right_proximal, right_distal
+    # ref_angles = np.array([0.,0.,0.,0.,0.,0.]) # spring reference angle left_proximal, left_distal, right_proximal, right_distal   
+
+    dc = 0.5
+    max_load = 5.
+    
+    def __init__(self):
+        rospy.init_node('SimAcrobotNode', anonymous=True)
+
+        #initialize service handlers:
+        rospy.Service('MoveServos', moveservo, self.MoveServosProxy)
+
+        rospy.Subscriber('/acrobot/my_joint_states', Float32MultiArray, self.JointStatesCallback)
+        rospy.Subscriber('/acrobot/my_joint_velocities', Float32MultiArray, self.JointVelCallback)
+
+        self.pub_j1 = rospy.Publisher('/acrobot/joint1_controller/command', Float64, queue_size=10)
+
+        self.gripper_load_pub = rospy.Publisher('/acrobot/load', Float32, queue_size=10)
+        msg = Float32()
+
+        rate = rospy.Rate(100)
+        while not rospy.is_shutdown():
+
+            d = self.set_damping_matrix()
+            self.act_torque = self.act_load - d * self.act_vel[0] # Damping does not work well because of numerical errors
+
+            self.pub_j1.publish(self.act_torque*self.max_load)
+            
+            msg.data = self.act_torque
+            self.gripper_load_pub.publish(msg)
+            
+            rate.sleep()
+
+    def set_damping_matrix(self):
+        # if np.abs(self.act_vel[0]) < 0.2e-2:
+        #     d = 0.0
+        # else:
+        #     if self.act_angles[0] > 0 and self.act_angles[0] < 2.5:
+        #         d = 1.23*self.dc
+        #     elif self.act_angles[0] < -0.34 and self.act_angles[0] > -0.88:
+        #         d = 1.59*self.dc
+        # else:
+        d = self.dc
+
+        return d
+
+    def JointStatesCallback(self, msg):
+        self.act_angles = np.array(msg.data)
+
+    def JointVelCallback(self, msg):
+        self.act_vel = np.array(msg.data)
+
+    def MoveServosProxy(self, req):
+        self.act_load = req.torque
+
+        # Enforce normalized actuator angles
+        self.act_load = min(self.act_load,1)
+        self.act_load = max(self.act_load,-1)
+
+        return 0
+        
+
+
+
+if __name__ == '__main__':
+
+    try:
+        SimAcrobotNode()
+    except rospy.ROSInterruptException:
+        pass
---
+++ acrobot_simulator/acrobot_controllers/src/my_joint_state_publisher.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python 
+
+'''
+----------------------------
+Author: Avishai Sintov
+        Rutgers University
+Date: October 2018
+----------------------------
+'''
+
+
+import rospy
+import numpy as np 
+from std_msgs.msg import Float64, Float32MultiArray
+from gazebo_msgs.msg import LinkStates
+from rosgraph_msgs.msg import Clock
+import PyKDL
+
+windowSize = 10
+
+class my_joint_state_publisher():
+
+    joint_angles = None#, dtype=np.float32)
+    joint_angles_prev = None
+    joint_vels = None
+    order = np.array([0,0,0,0,0,0,0,0,0])
+    msg = Float32MultiArray()
+    Gtype = None
+    current_time = 0.0
+    prev_time = 0.0
+    win = np.array([])
+
+    def __init__(self):
+        rospy.init_node('my_joint_state_publisher', anonymous=True)
+
+        self.joint_angles = [0.,0.]
+        self.joint_vels = [0.,0.]
+
+        rospy.Subscriber('/gazebo/link_states', LinkStates, self.linkStatesCallback)
+        rospy.Subscriber('/clock', Clock, self.ClockCallback)
+        joint_states_pub = rospy.Publisher('/acrobot/my_joint_states', Float32MultiArray, queue_size=10)
+        joint_vel_pub = rospy.Publisher('/acrobot/my_joint_velocities', Float32MultiArray, queue_size=10)
+
+        rate = rospy.Rate(100)
+        while not rospy.is_shutdown():
+            # self.UpdateVelocities() # Update is now done in linkStatesCallback(.)
+
+            self.msg.data = self.joint_angles
+            joint_states_pub.publish(self.msg)
+
+            self.msg.data = self.joint_vels
+            joint_vel_pub.publish(self.msg)   
+
+            rate.sleep()
+
+    def linkStatesCallback(self, msg):
+
+        self.getNameOrder(msg.name)
+
+        q_world = PyKDL.Rotation.Quaternion(msg.pose[self.order[0]].orientation.x, msg.pose[self.order[0]].orientation.y, msg.pose[self.order[0]].orientation.z, msg.pose[self.order[0]].orientation.w)
+        q_1 = PyKDL.Rotation.Quaternion(msg.pose[self.order[1]].orientation.x, msg.pose[self.order[1]].orientation.y, msg.pose[self.order[1]].orientation.z, msg.pose[self.order[1]].orientation.w)
+        q_2 = PyKDL.Rotation.Quaternion(msg.pose[self.order[2]].orientation.x, msg.pose[self.order[2]].orientation.y, msg.pose[self.order[2]].orientation.z, msg.pose[self.order[2]].orientation.w)
+        dq_1 = msg.twist[self.order[1]].angular.z
+        dq_2 = msg.twist[self.order[2]].angular.z
+    
+        if 1:
+            a = (q_1.Inverse()*q_world).GetEulerZYX()
+            if a[2] > 0:
+                self.joint_angles[0] = a[1]
+            elif a[0] < 0:
+                self.joint_angles[0] = np.pi - a[1]
+            else:
+                self.joint_angles[0] = -(np.pi + a[1])
+            b = (q_2.Inverse()*q_1).GetEulerZYX()
+            if b[0] > 3.13 or (b[0] < -3.13 and b[2] < -3.13):
+                self.joint_angles[1] = b[1]
+            elif b[0] < 0:
+                self.joint_angles[1] = np.pi - b[1]
+            else:
+                self.joint_angles[1] = -(np.pi + b[1])
+            
+            self.joint_angles[1] = self.joint_angles[1] - np.pi if self.joint_angles[1] > 0 else self.joint_angles[1] + np.pi
+
+            # Apply mean filter to joint velocities with windowSize
+            self.joint_vels = np.array([dq_1, (dq_2 - dq_1)])
+            # if self.win.shape[0] < windowSize:
+            #     self.joint_vels = np.array([-dq_1, -(dq_2 - dq_1)]) # Currently finger velocity do not compensate base motion
+            #     self.win = np.append(self.win, self.joint_vels).reshape(-1, 2)
+            # else:
+            #     v = np.array([-dq_1, -(dq_2 - dq_1)]) # Currently finger velocity do not compensate base motion
+            #     self.win = np.append(self.win, v).reshape(-1, 2)
+            #     self.joint_vels = np.mean(self.win, axis=0)
+            #     self.win = np.delete(self.win, 0, axis=0)  
+            self.joint_vels[np.abs(self.joint_vels) <= 0.1e-2] = 0.    
+
+    def ClockCallback(self, msg):
+        self.current_time = msg.clock.secs + msg.clock.nsecs * 1e-9
+
+    def getNameOrder(self, names):
+
+        for i, name in enumerate(names):
+            # print(i,str)
+            if name.find('ground_plane::link') > 0:
+                self.order[0] = i
+                continue
+            if name.find('link1') > 0:
+                self.order[1] = i
+                continue
+            if name.find('link2') > 0:
+                self.order[2] = i
+                continue
+
+if __name__ == '__main__':
+
+    try:
+        my_joint_state_publisher()
+    except rospy.ROSInterruptException:
+        pass
